527eaa387ec3c41b91614b5dd57fbdd1ebc71bec
Rajeshwari05-V <rajeshwari201005@gmail.com>
2026-06-03 21:07:58 +0530

    Add data augmentation module

--- member5-cv/preprocessing/augment.py
+++ member5-cv/preprocessing/augment.py
@@ -1,1 +1,14 @@
+import albumentations as A
 
+def get_augmentation_pipeline():
+
+    return A.Compose([
+        A.RandomRotate90(p=0.5),
+        A.HorizontalFlip(p=0.5),
+        A.VerticalFlip(p=0.3),
+        A.RandomBrightnessContrast(
+            brightness_limit=0.2,
+            contrast_limit=0.2,
+            p=0.5
+        ),
+    ])
