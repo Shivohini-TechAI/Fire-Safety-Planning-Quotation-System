8b3cbc1bf7f33603a1eb3d1d6f52d0b0c33addbf
Rajeshwari05-V <rajeshwari201005@gmail.com>
2026-06-03 21:11:31 +0530

    Add evaluation module

--- member5-cv/detection/evaluate.py
+++ member5-cv/detection/evaluate.py
@@ -1,1 +1,12 @@
+from ultralytics import YOLO
 
+def evaluate():
+
+    model = YOLO("models/best.pt")
+
+    results = model.val()
+
+    print(results)
+
+if __name__ == "__main__":
+    evaluate()
