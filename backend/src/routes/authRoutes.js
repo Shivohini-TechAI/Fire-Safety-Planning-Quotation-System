74def37849599c9c7520aed06ea9e6d0052b234c
gokulpujari099@gmial.com <gokulpujari099@gmail.com>
2026-07-07 20:52:08 +0530

    Complete user management, role authorization and add Swagger dependencies

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const router = express.Router();
 
+console.log("authRoutes loaded");
+
 const {
     signup,
     login,
@@ -11,6 +13,7 @@ const {
 } = require("../controllers/authController");
 
 const verifyToken = require("../middleware/authMiddleware");
+const authorizeRole = require("../middleware/authorizeRole");
 
 router.post("/signup", signup);
 router.post("/login", login);
@@ -20,5 +23,21 @@ router.get("/profile", verifyToken, getProfile);
 router.put("/profile", verifyToken, updateProfile);
 
 router.put("/change-password", verifyToken, changePassword);
+
+// Admin Only Route
+router.get("/admin", verifyToken, authorizeRole("admin"), (req, res) => {
+    res.json({
+        message: "Welcome Admin!",
+        user: req.user
+    });
+});
+
+// User Only Route
+router.get("/user", verifyToken, authorizeRole("user"), (req, res) => {
+    res.json({
+        message: "Welcome User!",
+        user: req.user
+    });
+});
 
 module.exports = router;
