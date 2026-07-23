adf47c00fc5d11df28b82d95da2dcd90bacd9f4c
Harsh <harshvardhand351@gmail.com>
2026-07-23 15:47:57 +0530

    Add CORS middleware to backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,14 @@
 require("dotenv").config()
 
 const express = require("express");
+const cors = require("cors");
 require("./config/db");
 
 const swaggerUi = require("swagger-ui-express");
 const swaggerSpec = require("./swagger");
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 const authRoutes = require("./routes/authRoutes");
